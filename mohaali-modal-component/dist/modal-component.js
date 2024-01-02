aa6013dc3c6de1e4d2cc436e346227954ae62473
mohaali77 <mzr.mohand@gmail.com>
2024-01-03 00:17:54 +0100

    Update modal-component.js

--- mohaali-modal-component/dist/modal-component.js
+++ mohaali-modal-component/dist/modal-component.js
@@ -7,24 +7,24 @@ exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./modal-component.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+// lib/modal-component.js
+
 const ModalComponent = _ref => {
   let {
-    type = "text",
-    label,
-    value,
-    onChange
+    isOpen,
+    onClose,
+    children
   } = _ref;
+  if (!isOpen) {
+    return null;
+  }
   return /*#__PURE__*/_react.default.createElement("div", {
-    className: "simple-form-group"
-  }, label && /*#__PURE__*/_react.default.createElement("label", {
-    className: "simple-text-label"
-  }, label), /*#__PURE__*/_react.default.createElement("div", {
-    className: "test"
-  }, "Ceci est un test"), /*#__PURE__*/_react.default.createElement("input", {
-    type: type,
-    className: "simple-text-input",
-    value: value,
-    onChange: e => onChange && onChange(e.target.value)
-  }));
+    className: "modal-overlay"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal"
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    className: "close-button",
+    onClick: onClose
+  }, "Close"), children));
 };
 var _default = exports.default = ModalComponent;
