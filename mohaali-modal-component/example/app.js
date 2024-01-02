b6ca1e5ea1b631ff869e65d45fc7a27e876330fd
mohaali77 <mzr.mohand@gmail.com>
2024-01-03 00:17:58 +0100

    Create app.js

---
+++ mohaali-modal-component/example/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ModalComponent from '../dist/modal-component.js';
+
+ReactDOM.render(<ModalComponent />, document.getElementById('root'));
