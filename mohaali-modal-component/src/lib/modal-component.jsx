9cf605bb79cb0695a8fafc60ca25d826b54dc5c9
mohaali77 <mzr.mohand@gmail.com>
2024-01-03 13:06:59 +0100

    Update modal-component.jsx

--- mohaali-modal-component/src/lib/modal-component.jsx
+++ mohaali-modal-component/src/lib/modal-component.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import './modal-component.css';
 import closeImg from './images/modal-close.png'
 
-const ModalComponent = ({ isOpen, onClose, modalContent }) => {
+const ModalComponent = ({ isOpen, onClose, modalContent, overlayStyle, modalStyle }) => {
   if (!isOpen) {
     return null;
   }
 
   return <>
-    <div className="modal-overlay">
+    <div style={overlayStyle} className="modal-overlay">
       <div style={modalStyle} className="modal">
         <p> {modalContent}Employee Created !</p>
         <img src={closeImg} alt='' className="close-button" onClick={onClose} />
