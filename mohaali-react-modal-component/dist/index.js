8df8171150101b35630cbf6fcf2f028aa9e961b8
mohaali77 <mzr.mohand@gmail.com>
2024-01-04 23:45:21 +0100

    Create index.js

---
+++ mohaali-react-modal-component/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _modalComponent = _interopRequireDefault(require("./modal-component.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _default = exports.default = _modalComponent.default;
