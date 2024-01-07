101f13b64fd9957677b179d75180ae54ba2d34bb
mohaali77 <mzr.mohand@gmail.com>
2024-01-07 14:45:20 +0100

    Update index.js

--- mohaali-react-modal-component/src/index.js
+++ mohaali-react-modal-component/src/index.js
@@ -5,19 +5,21 @@ import ModalComponent from './lib/modal-component';
 const App = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
+  const closeModal = () => {
+    setModalOpen(false)
+    window.location.href = '/autrePage'
+  };
+
   return (
     <div>
       <h1>Mon application React</h1>
       <button
-        onClick={() => {
-          setModalOpen(true);
-        }}
+        onClick={() => { setModalOpen(true); }}
 
       >Ouvrir la modal</button>
       <ModalComponent
         isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-      />
+        onClose={closeModal} />
     </div>
   );
 };
