7a76da0c804adc93fdf20a7028f0ef58e053d69a
mohaali77 <mzr.mohand@gmail.com>
2024-02-11 19:39:14 +0100

    Update modal-component.js

--- mohaali-react-modal-component/src/lib/modal-component.js
+++ mohaali-react-modal-component/src/lib/modal-component.js
@@ -1,9 +1,33 @@
+/**
+ * Composant de modal.
+ * @module ModalComponent
+ */
+
+
 import React from 'react';
 import './modal-component.css';
 import closeImg from './images/modal-close.png'
 
+
+/**
+ * Composant de modal réutilisable.
+ * @param {object} props - Les propriétés du composant.
+ * @param {boolean} props.isOpen - Indique si la modal est ouverte ou fermée.
+ * @param {string} props.modalContent - Le contenu de la modal.
+ * @param {object} props.overlayStyle - Style de l'overlay de la modal.
+ * @param {object} props.modalStyle - Style de la modal elle-même.
+ * @param {object} props.contentStyle - Style du contenu de la modal.
+ * @param {object} props.imgStyle - Style de l'image du bouton de fermeture.
+ * @param {function} props.onClose - Fonction de rappel appelée lors de la fermeture de la modal.
+ * @returns {JSX.Element} Composant de modal.
+ */
 const ModalComponent = ({ isOpen, modalContent, overlayStyle, modalStyle, contentStyle, imgStyle, onClose }) => {
 
+
+  /**
+   * Condition qui va afficher la modal si elle est ouverte, sinon elle ne retournera null.
+   * @returns {JSX.Element|null} 
+   */
   if (!isOpen) {
     return null;
   }
