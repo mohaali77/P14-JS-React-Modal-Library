a88179638bf2830dc7cd13827cd3928c92e7ad9f
mohaali77 <mzr.mohand@gmail.com>
2024-02-11 19:39:17 +0100

    Update index.js

--- mohaali-react-modal-component/src/index.js
+++ mohaali-react-modal-component/src/index.js
@@ -1,10 +1,26 @@
+/**
+ * Composant principal de l'application React.
+ * @module App
+ */
+
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import ModalComponent from './lib/modal-component';
 
+/**
+ * Composant principal de l'application.
+ * @returns {JSX.Element} Composant principal de l'application.
+ */
 const App = () => {
+  /**
+   * État pour contrôler l'ouverture et la fermeture de la modal.
+   * @type {[boolean, function]} Le tableau contient l'état de la modal et la fonction pour changer son état.
+   */
   const [modalOpen, setModalOpen] = useState(false);
 
+  /**
+   * Fonction pour fermer la modal et, si souhaité, rediriger vers une page choisie.
+   */
   const closeModal = () => {
     setModalOpen(false)
     window.location.href = '/autrePage'
